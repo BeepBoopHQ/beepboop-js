9fcdf77dedfaa9c769d92b0a21cc66a16f23627c
Brad Harris <bmharris@gmail.com>
2016-04-26 09:08:10 -0600

    updating tests to use sinon.spy for brevity

--- test/resourcer_test.js
+++ test/resourcer_test.js
@@ -6,7 +6,6 @@ var Resourcer = require('../lib/resourcer.js')
 describe('Resourcer', function () {
   var socket
   var resourcer
-  var called
 
   beforeEach(function () {
     resourcer = new Resourcer()
@@ -16,7 +15,6 @@ describe('Resourcer', function () {
     sinon.stub(resourcer, 'newWebSocket').returns(socket)
 
     resourcer.connect(socket)
-    called = false
   })
 
   afterEach(function () {
@@ -25,6 +23,7 @@ describe('Resourcer', function () {
 
   describe('web socket connection event handling', function () {
     it('handles error event', function () {
+      var called = false
       resourcer.on('error', function (err) {
         called = true
         assert.equal(err, 'error')
@@ -35,67 +34,54 @@ describe('Resourcer', function () {
     })
 
     it('handles open event', function () {
-      resourcer.on('open', function () {
-        called = true
-      })
+      var spy = sinon.spy(resourcer, 'emit')
 
       socket.emit('open')
-      assert.equal(called, true)
+      assert(spy.calledWith('open'))
     })
 
     it('handles close event', function () {
-      resourcer.on('close', function (code, message) {
-        called = true
-        assert.equal(code, 1000)
-        assert.equal(message, 'closed')
-      })
+      var spy = sinon.spy(resourcer, 'emit')
 
       socket.emit('close', 1000, 'closed')
-      assert.equal(called, true)
+      assert(spy.calledWith('close', 1000, 'closed'))
     })
 
     describe('message events', function () {
       it('handles add_resource event', function () {
-        resourcer.on('add_resource', function (msg) {
-          called = true
-          assert.deepEqual(msg, {'type': 'add_resource', 'ResourceID': '4zewnkfyldi'})
-        })
+        var spy = sinon.spy(resourcer, 'emit')
+        var message = {'type': 'add_resource', 'ResourceID': '4zewnkfyldi'}
 
-        socket.emit('message', '{"type": "add_resource", "ResourceID":"4zewnkfyldi"}')
-        assert.equal(called, true)
+        socket.emit('message', JSON.stringify(message))
+        assert.equal(spy.calledWith('add_resource', message), true)
       })
 
       it('handles update_resource event', function () {
-        resourcer.on('update_resource', function (msg) {
-          called = true
-          assert.deepEqual(msg, {'type': 'update_resource', 'ResourceID': '4zewnkfyldi-update'})
-        })
+        var spy = sinon.spy(resourcer, 'emit')
+        var message = {'type': 'update_resource', 'ResourceID': '4zewnkfyldi-update'}
 
-        socket.emit('message', '{"type": "update_resource", "ResourceID":"4zewnkfyldi-update"}')
-        assert.equal(called, true)
+        socket.emit('message', JSON.stringify(message))
+        assert.equal(spy.calledWith('update_resource', message), true)
       })
 
       it('handles remove_resource event', function () {
-        resourcer.on('remove_resource', function (msg) {
-          called = true
-          assert.deepEqual(msg, {'type': 'remove_resource', 'ResourceID': '4zewnkfyldi-update'})
-        })
+        var spy = sinon.spy(resourcer, 'emit')
+        var message = {'type': 'remove_resource', 'ResourceID': '4zewnkfyldi-update'}
 
-        socket.emit('message', '{"type": "remove_resource", "ResourceID":"4zewnkfyldi-update"}')
-        assert.equal(called, true)
+        socket.emit('message', JSON.stringify(message))
+        assert.equal(spy.calledWith('remove_resource', message), true)
       })
 
       it('handles auth_result event', function () {
-        resourcer.on('auth_result', function (msg) {
-          called = true
-          assert.deepEqual(msg, {'type': 'auth_result', 'ResourceID': '4zewnkfyldi-update', 'success': 'true'})
-        })
+        var spy = sinon.spy(resourcer, 'emit')
+        var message = {'type': 'auth_result', 'ResourceID': '4zewnkfyldi-update', 'success': 'true'}
 
-        socket.emit('message', '{"type": "auth_result", "ResourceID":"4zewnkfyldi-update", "success": "true"}')
-        assert.equal(called, true)
+        socket.emit('message', JSON.stringify(message))
+        assert.equal(spy.calledWith('auth_result', message), true)
       })
 
       it('handles a bad message', function () {
+        var called = false
         resourcer.on('error', function () {
           called = true
         })
@@ -105,7 +91,7 @@ describe('Resourcer', function () {
       })
 
       it('does not emit an error if listener throws', function () {
-        var spy = sinon.spy(resourcer, 'on')
+        var spy = sinon.spy(resourcer, 'emit')
         resourcer.on('auth_result', function () {
           throw new Error('kaboom')
         })
